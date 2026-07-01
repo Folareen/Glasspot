624fec0a1526feab3ac2c7f0c6979149c9993373
Adedunmola Oyewale <oyewaleadedunmola@gmail.com>
2026-07-01 16:39:48 +0100

    register auth routes

--- apps/api/src/app.ts
+++ apps/api/src/app.ts
@@ -3,6 +3,7 @@ import Fastify, { type FastifyInstance } from "fastify";
 import { healthRoutes } from "./routes/health.js";
 import jwtPlugin from "./lib/plugins/jwt.js";
 import env from "./config/env.js";
+import authRoutes from "./modules/auth/auth.route.js";
 
 export function buildApp(): FastifyInstance {
   const app = Fastify({ logger: true });
@@ -10,6 +11,7 @@ export function buildApp(): FastifyInstance {
   app.register(jwtPlugin);
   app.register(cors, { origin: env.WEB_ORIGIN ?? "http://localhost:3000" });
   app.register(healthRoutes);
+  app.register(authRoutes, { prefix: "/auth" });
 
   return app;
 }
